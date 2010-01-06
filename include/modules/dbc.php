6ce1f04d1771d7b499fd4b2789d1bdc6c6b500ce
Frank <61924.00@gmail.com>
2010-01-06 21:09:34 +0100

    The database module.

---
+++ include/modules/dbc.php
@@ -0,0 +1,54 @@
+<?php
+
+# =============================================================================
+# include/modules/db.php
+#
+# Copyright (c) 2009 Frank Smit
+# License: zlib/libpng, see the COPYING file for details
+# =============================================================================
+
+class dbc
+{
+	public $c = false;
+
+	public function __construct()
+	{
+		global $db_type, $db_host, $db_name, $db_user, $db_password;
+
+		$this->connect($db_type, $db_host, $db_name, $db_user, $db_password);
+	}
+
+	public function connect($db_type, $db_host, $db_name, $db_user, $db_password)
+	{
+		try
+		{
+			switch ($db_type)
+			{
+				case 'mysql':
+					$this->c = new PDO('mysql:host='.$db_host.';dbname='.$db_name, $db_user, $db_password,
+						array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+					break;
+				case 'pgsql':
+					$this->c = new PDO('pgsql:host='.$db_host.';dbname='.$db_name, $db_user, $db_password);
+					break;
+				case 'sqlite2':
+					$this->c = new PDO('sqlite2:'.$db_name);
+					break;
+				case 'sqlite':
+					$this->c = new PDO('sqlite:'.$db_name);
+					break;
+				default:
+					error('There is no support for the specified database type, "'.$db_type.'".');
+			}
+		}
+		catch (PDOException $e)
+		{
+			error($e->getMessage(), __FILE__, __LINE__);
+		}
+	}
+
+	public function close()
+	{
+		$this->c = false;
+	}
+}
