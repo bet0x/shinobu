1656d8440f383ce604815e4b9765ac937a4ada96
Frank Smit <61924.00@gmail.com>
2010-02-28 16:54:22 +0100

    'Groups' administration main page is done. Edit, Delete and Add actions still have to be added.

---
+++ controllers/admin/groups/default.php
@@ -0,0 +1,41 @@
+<?php
+
+# =============================================================================
+# controllers/admin/groups/default.php
+#
+# Copyright (c) 2009-2010 Frank Smit
+# License: zlib/libpng, see the COPYING file for details
+# =============================================================================
+
+class default_controller extends AuthWebController
+{
+	public function prepare()
+	{
+		if (!$this->user->authenticated() || !$this->acl->get('administration', ACL_PERM_6))
+			$this->redirect(SYSTEM_BASE_URL);
+	}
+
+	public function GET($args)
+	{
+		// Get usergroups
+		$usergroups = array();
+		$result = $this->db->query('SELECT g.id, g.name, g.description, COUNT(u.group_id) AS user_count FROM '.DB_PREFIX.'usergroups AS g '.
+		                           'LEFT JOIN '.DB_PREFIX.'users AS u ON u.group_id=g.id GROUP BY g.id')
+			or error('Unable to fetch usergroups.', __FILE__, __LINE__);
+
+		if ($this->db->num_rows($result) > 0)
+		{
+			while ($row = $this->db->fetch_assoc($result))
+				$usergroups[] = $row;
+		}
+
+		return tpl::render('admin_groups', array(
+			'website_section' => 'Administration',
+			'page_title' => 'Groups',
+			'subsection' => 'groups',
+			'admin_perms' => $this->acl->get('administration'),
+
+			'usergroups' => $usergroups
+			));
+	}
+}
--- templates/admin_groups.php
+++ templates/admin_groups.php
@@ -4,7 +4,52 @@
 <div id="admin-content">
 	<h2>Groups</h2>
 
-	<?php echo $page_body ?>
+	<p>Usergroups can be used to groups users and give those groups certain permissions to give the members of those groups permission to do
+	   certain tasks or give them access to restricted content.</p>
+
+	<p class="note">Note: Groups that contain users can not be deleted.</p>
+
+	<ul class="two-column-list">
+	<?php foreach ($usergroups as $index => $group): ?>
+		<li class="row-<?php echo $index % 2 ? 'odd' : 'even' ?>">
+			<div class="name"><strong><?php echo u_htmlencode($group['name']) ?></strong></div>
+			<div class="description"><?php echo u_htmlencode($group['description']) ?></div>
+			<div class="actions">
+				<a href="<?php echo utils::url('admin/groups/edit/'.$group['id']) ?>">Edit</a> &middot;
+				<?php if ($group['user_count'] > 0): ?>
+				<span class="disabled-link">Delete</span>
+				<?php else: ?>
+				<a href="<?php echo utils::url('admin/groups/delete/'.$group['id']), '?', utils::xsrf_token() ?>">Delete</a>
+				<?php endif ?>
+			</div>
+		</li>
+	<?php endforeach ?>
+	</ul>
+
+	<h3>Add new group</h3>
+
+	<p>Both fields are required. You will be redirected to a page where you can configure the permissions and usertitle for the group
+	   after you completed both fields and pressed <em>Add</em>.</p>
+
+	<form  class="form-style-two" method="post" accept-charset="utf-8" action="<?php echo utils::url('admin/groups/add') ?>">
+		<div>
+			<?php echo utils::xsrf_form_html(), "\n" ?>
+		</div>
+		<p>
+			<label>
+				<strong>Name <span>(required)</span></strong>
+				<input type="text" name="form[name]" maxlength="20" />
+			</label>
+			<label>
+				<strong>Description <span>(required)</span></strong>
+				<input type="text" name="form[description]" maxlength="255" />
+			</label>
+		</p>
+		<p class="buttons">
+			<input type="submit" value="Add" name="form_admin_add_group" />
+			<input type="reset" value="Reset" />
+		</p>
+	</form>
 </div>
 
 <div class="clear">&nbsp;</div>
