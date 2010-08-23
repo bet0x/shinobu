cc0d2e64a05ae6a4a89dac9057c77422b07aecf3
Frank <frank@61924.nl>
2010-08-23 22:15:08 +0200

    Added php-utf8 as a submodule.

--- index.php
+++ index.php
@@ -41,7 +41,7 @@ if (get_magic_quotes_gpc())
 // Unset any variables instantiated as a result of register_globals being enabled
 unregister_globals();
 
-if (defined('UTF8'))
+/*if (defined('UTF8'))
 {
 	// Check whether PCRE has been compiled with UTF-8 support
 	$UTF8_ar = array();
@@ -59,7 +59,7 @@ if (defined('UTF8_USE_MBSTRING'))
 	to 0, 1 or 4 in php.ini (string overloading disabled).
 	Also need to check we have the correct internal mbstring
 	encoding. */
-	if (ini_get('mbstring.func_overload') & MB_OVERLOAD_STRING)
+	/*if (ini_get('mbstring.func_overload') & MB_OVERLOAD_STRING)
 		trigger_error('String functions are overloaded by mbstring', E_USER_ERROR);
 
 	mb_language('uni');
@@ -75,7 +75,9 @@ elseif (defined('UTF8_USE_NATIVE'))
 		require UTF8.'/utils/unicode.php';
 		require UTF8.'/native/core.php';
 	}
-}
+}*/
+
+require UTF8.'/php-utf8.php';
 
 // Load classes
 require SYS_INCLUDE.'/classes.php';
